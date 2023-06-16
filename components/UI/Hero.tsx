79a55902cccf6787967c304f0ec4e720988eb088
aliamirk <ali.bussiness01@gmail.com>
2023-06-16 23:58:02 +0500

    create hero

--- components/UI/Hero.tsx
+++ components/UI/Hero.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import { images } from "../../public/assests";
 import foodimage from "../../public/assests/foodcoverimage.jpg";
-import Wrapper from "../Wrapper/Wrapper";
+import Wrapper from "../Containers/Wrapper";
 
 const Hero = () => {
 
     return (
         <Wrapper>
-        <Image src={foodimage} alt="Cover Food Image" width={1000} height={100} className="w-[100%] h-[60vh]  md:h-[70vh]  object-fill  "/>
+        <Image src={foodimage} alt="Cover Food Image" width={1000} height={100} className="w-[100%] h-[60vh]  md:h-[70vh]  object-fill"/>
+        <div className="max-w-6xl mt-[-250px] text-2xl md:mt-[-350px] mx-auto text-center md:text-4xl font-semibold text-slate-400 ">
+        Frontier is the ultimate destination for food enthusiasts, offering a diverse array of cuisines and flavors from around the world
+        </div>
         </Wrapper>
     )
 };
--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -1,30 +1,36 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Wrapper from "../Wrapper/Wrapper";
+import Wrapper from "../Containers/Wrapper";
 import CartButton from "./CartButton";
-
 
 const Navbar = () => {
   return (
     // MLLM
     <Wrapper>
       <nav className="flex bg-slate-800 text-white px-4 justify-between items-center h-16 z-10 sticky">
-
         <div className="flex gap-24 ">
-        {/* Logo */}
-          <div className="font-bold text-[18px] tracking-widest">
-            Frontier
-          </div>
+          {/* Logo */}
+          <div className="font-bold text-[18px] tracking-widest">Frontier</div>
           <div className="flex-row hidden md:block md:gap-6 md:space-x-4">
-          <Link href="/">Home</Link>
-          <Link href="/About">About</Link>
+            <Link
+              href="/"
+              className=" hover:border-b-slate-500 hover:border-b-2  duration-500 transition"
+            >
+              Home
+            </Link>
+            <Link
+              href="/About"
+              className=" hover:border-b-slate-500 hover:border-b-2  duration-500 transition"
+            >
+              About
+            </Link>
           </div>
         </div>
-      
+
         <div>
           {/* Cart */}
-          <CartButton className="rounded-full bg-slate-600"/>
+          <CartButton className="rounded-full bg-slate-600" />
         </div>
       </nav>
     </Wrapper>
